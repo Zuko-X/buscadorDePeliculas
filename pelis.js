3b01a087e25bd839c23adc1f146445993389e7b2
Skaysz <thomas680159753@gmail.com>
2023-05-01 18:23:29 -0300

    Changes functions

--- pelis.js
+++ pelis.js
@@ -1,18 +1,22 @@
-
 const fs = require("fs");
+const { get } = require("http");
+const lista = fs.readFileSync(__dirname + "/pelis.json")
+
 // Tiene que leer el archivo JSON y exponer funciones para interactuar con los datos.
+// fetch("./pelis.json")
+// .then(res => res.json())
+// .then(lista => console.log(lista))
+// const lista = JSON.parse(lista)
+
+exports.getAll = () => {
+    const todasLasPelis = lista.peliculas
+    return todasLasPelis;
+}
 
 
-const data = fs.readFileSync(__dirname +"/pelis.json");
 
-const lista = JSON.parse(data)
 exports.buscadorDePelis = () => {
-    for (let i = 0; i < lista.peliculas.length; i++) {
-        const tags = lista.peliculas[i].tags;
-
-        // Mostramos los tags en la consola
-        console.log(`Tags de la película ${i+1}: ${tags.join(", ")}`);
-    }
+    
 }
 
 exports.sort = () => {
@@ -23,8 +27,26 @@ exports.search = () => {
   
 }
 
-// exports.tag = (data) => {
-//     if (data === lista.peliculas[1])
-// }
+exports.searchByTag = (tag) => {
+    for (let i = 0; i < lista.peliculas.length; i++) {
+        const tags = lista.peliculas[i].tags;
+            switch (tag) {
+                case "animación":
+                  peliculasAnimacion.push(lista.peliculas[i]);
+                  break;
+                case "aventura":
+                    
+                    break;
+                case "familia":
 
-console.log(buscadorDePelis())
+                    break;
+                default:
+                    break;
+            };
+        // Mostramos los tags en la consola
+        console.log(`Tags de la película ${i+1}: ${tags.join(", ")}`);
+    }
+    return peliculasAnimacion
+}
+
+console.log(this.getAll())
