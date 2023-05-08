16b80f475d0b9510c303b815e8c0a73a8a2c04c4
MorePC <thomas680159753@gmail.com>
2023-05-08 02:49:39 -0300

    -Fix sort()

--- pelis.js
+++ pelis.js
@@ -9,7 +9,7 @@ const listaPelis = JSON.parse(lista);
 // const lista = JSON.parse(lista)
 
 exports.getAll = () => {
-    let lista = [];
+    const lista = [];
     const peliculas = listaPelis.peliculas
     for (let i = 0; i < peliculas.length; i++) {
         lista.push(listaPelis.peliculas[i])
@@ -17,36 +17,86 @@ exports.getAll = () => {
     return lista;
 }
 
-exports.sort = () => {
-  
+exports.sort = (texto) => {
+    let lista = [];
+    const peliculas = listaPelis.peliculas;
+    if (texto === "title") {
+        for (let i = 0; i < peliculas.length; i++) {
+            lista.push(peliculas[i].title);
+        }
+        lista.sort();
+    }
+    else if (texto === "rating") { //no me muestra correctamente ordenado por rating
+        lista = peliculas.map((p) => ({ title: p.title, rating: p.rating })); // crea un array de objetos con título y rating
+        lista.sort((a, b) => b.rating - a.rating); // ordena de mayor a menor según el rating
+        listaFix = lista.map((p) => ({title: p.title, rating: p.rating.toFixed(0) + "%"}))
+        return listaFix;
+    } 
+    else if (texto === "year") {
+        lista = peliculas.map((p) => ({ title: p.title, year: p.year }));
+        lista.sort((a, b) => b.year - a.year);
+    }
+    else if (texto === "runtime") { //No me muestra correctamente ordenado por runtime
+        const listaOrdenada = peliculas.map((p) => {
+            const duracion = p.runtime;
+            const partes = duracion.split(" "); // divide el string en dos partes: "1h" y "32m"
+            const horas = parseInt(partes[0]); // extrae el número de horas y conviértelo en un entero
+            const minutos = parseInt(partes[1]); // extrae el número de minutos y conviértelo en un entero
+            const minutosTotales = horas * 60 + minutos; // calcula el total de minutos
+            return { title: p.title, minutosTotales: minutosTotales };
+        }); // crea una lista de objetos que incluye el título de la película y la duración total en minutos
+        listaOrdenada.sort((a, b) => a.minutosTotales - b.minutosTotales); // ordena la lista según la duración total en minutos
+        const listaFix = listaOrdenada.map((p) => ({ title: p.title, runtime: `${Math.floor(p.minutosTotales / 60)}h ${p.minutosTotales % 60}m` })); // crea una lista de objetos que incluye el título de la película y la duración en formato "Xh Ym"
+        return listaFix;
+    }
+    else {
+        console.error(`Valor "${texto}" es invalido. Por favor ingresar title, rating, year, runtime.`)
+        return null;
+    }
+    return lista;
 }
 
 //Tengo que darle el titulo de la peli y entregarme los resultados similares
-exports.search = () => {
-  
+exports.search = (texto) => {
+    const peliculas = listaPelis.peliculas;
+    const peliculasMinuscula = peliculas.toLowerCase;
+    if (texto === undefined) {
+        console.error("Error. Se esperaba algun valor.");
+        return null;
+    } else {
+        let lista = []
+        lista = peliculas.filter((p) => p.title.toLowerCase().includes(texto.toLowerCase()));
+        return lista;
+    }
 }
 
 
-// Buscar una solucion
+// Arreglar y completar
+// Hay una forma de hacerlo sin switch, solo con .includes que lo hace mas corto el código
 exports.searchByTag = (tag) => {
-    for (let i = 0; i < lista.peliculas.length; i++) {
-        const tags = lista.peliculas[i].tags;
-            switch (tag) {
-                case "animación":
-                  peliculasAnimacion.push(lista.peliculas[i]);
-                  break;
-                case "aventura":
-                    
-                    break;
-                case "familia":
-
-                    break;
-                default:
-                    break;
-            };
-        // Mostramos los tags en la consola
-        console.log(`Tags de la película ${i+1}: ${tags.join(", ")}`);
+    let lista = [];
+    for (let i = 0; i < listaPelis.peliculas.length; i++) {
+        const tags = listaPelis.peliculas[i].tags;
+        switch (tag) {
+            case "animación":
+                if (tags.includes("animación")) {
+                    lista.push(listaPelis.peliculas[i]);
+                }
+                break;
+            case "aventura":
+                if (tags.includes("aventura")) {
+                    lista.push(listaPelis.peliculas[i]);
+                }
+                break;
+            case "familia":
+                if (tags.includes("familia")) {
+                    lista.push(listaPelis.peliculas[i]);
+                }
+                break;
+            default:
+                break;
+        }
     }
-    return peliculasAnimacion
+    return lista;
 }
 
