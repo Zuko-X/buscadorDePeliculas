59fcf3fdcdfa7a5392124227b3a75c96c25b62ac
Skaysz <thomas680159753@gmail.com>
2023-11-20 15:53:57 -0300

    Eliminado 2 lineas no utilizadas y simplificando export.getAll

--- index.js
+++ index.js
@@ -1,51 +1,44 @@
 //Recibe argumentos, los procesa y delega las acciones a pelis.js.
-const { parse } = require("path");
-const pelis = require("./pelis")
+const pelis = require("./pelis");
 
 function parsearTerminos(texto) {
-    if (typeof texto === "string" && texto.trim() !== "") {
-        const textoParseado = texto.toLowerCase();
-        return textoParseado;
-    } else {
-        return texto;
-    }
+	if (typeof texto === "string" && texto.trim() !== "") {
+		const textoParseado = texto.toLowerCase();
+		return textoParseado;
+	} else {
+		return texto;
+	}
 }
 
 const takeArguments = (texto) => {
-    texto = texto === undefined ? texto = "" : texto;
-    const args = texto.replace(/^--/, "");
-    if (args === "sort") {
-        return pelis.sort(process.argv[3]);
-    } 
-    else if (args === "search") {
-        return pelis.search(process.argv[3]);
-    }
-    else if (args === "tag") {
-        return pelis.searchByTag(process.argv[3]);
-    }
-    else if (args === "help") {
-        console.log("--sort")
-        console.log("--search")
-        console.log("--tag")
-        return null;
-    }
-    else if (args === "") {
-        return pelis.getAll();
-    }
-    else {
-        console.error(`El argumento "${args}" no es válido.`);
-        return null;
-    }
-}
+	texto = texto === undefined ? (texto = "") : texto;
+	const args = texto.replace(/^--/, "");
+	if (args === "sort") {
+		return pelis.sort(process.argv[3]);
+	} else if (args === "search") {
+		return pelis.search(process.argv[3]);
+	} else if (args === "tag") {
+		return pelis.searchByTag(process.argv[3]);
+	} else if (args === "help") {
+		console.log("--sort");
+		console.log("--search");
+		console.log("--tag");
+		return null;
+	} else if (args === "") {
+		return pelis.getAll();
+	} else {
+		console.error(`El argumento "${args}" no es válido.`);
+		return null;
+	}
+};
 
 function main() {
-    const element = takeArguments(parsearTerminos(process.argv[2]));
-    if (element === null) {
-        return;
-    }
-    else {
-    console.table(element);
-    }
+	const element = takeArguments(parsearTerminos(process.argv[2]));
+	if (element === null) {
+		return;
+	} else {
+		console.table(element);
+	}
 }
 
-main()
+main();
--- pelis.js
+++ pelis.js
@@ -1,77 +1,84 @@
 const fs = require("fs");
-const { get } = require("http");
-const lista = fs.readFileSync(__dirname + "/pelis.json")
+const lista = fs.readFileSync(__dirname + "/pelis.json");
 const listaPelis = JSON.parse(lista);
 
 exports.getAll = () => {
-    const lista = [];
-    const peliculas = listaPelis.peliculas
-    for (let i = 0; i < peliculas.length; i++) {
-        lista.push(listaPelis.peliculas[i])
-    }
-    return lista;
-}
+	const lista = [];
+	const peliculas = listaPelis.peliculas;
+	for (let i = 0; i < peliculas.length; i++) {
+		lista.push(listaPelis.peliculas[i]);
+	}
+	return peliculas;
+};
 
 exports.sort = (texto) => {
-    let lista = [];
-    const peliculas = listaPelis.peliculas;
-    if (texto === "title") {
-        for (let i = 0; i < peliculas.length; i++) {
-            lista.push(peliculas[i].title);
-        }
-        lista.sort();
-    }
-    else if (texto === "rating") {
-        lista = peliculas.map((p) => ({ title: p.title, rating: p.rating })); // crea un array de objetos con título y rating
-        lista.sort((a, b) => b.rating - a.rating); // ordena de mayor a menor según el rating
-        listaFix = lista.map((p) => ({title: p.title, rating: p.rating.toFixed(0) + "%"}))
-        return listaFix;
-    } 
-    else if (texto === "year") {
-        lista = peliculas.map((p) => ({ title: p.title, year: p.year }));
-        lista.sort((a, b) => b.year - a.year);
-    }
-    else if (texto === "runtime") { 
-        const listaOrdenada = peliculas.map((p) => {
-            const duracion = p.runtime;
-            const partes = duracion.split(" "); // divide el string en dos partes: "1h" y "32m"
-            const horas = parseInt(partes[0]); // extrae el número de horas y conviértelo en un entero
-            const minutos = parseInt(partes[1]); // extrae el número de minutos y conviértelo en un entero
-            const minutosTotales = horas * 60 + minutos; // calcula el total de minutos
-            return { title: p.title, minutosTotales: minutosTotales };
-        }); // crea una lista de objetos que incluye el título de la película y la duración total en minutos
-        listaOrdenada.sort((a, b) => a.minutosTotales - b.minutosTotales); // ordena la lista según la duración total en minutos
-        const listaFix = listaOrdenada.map((p) => ({ title: p.title, runtime: `${Math.floor(p.minutosTotales / 60)}h ${p.minutosTotales % 60}m` })); // crea una lista de objetos que incluye el título de la película y la duración en formato "Xh Ym"
-        return listaFix;
-    }
-    else {
-        console.error(`Valor "${texto}" es invalido. Por favor ingresar title, rating, year, runtime.`)
-        return null;
-    }
-    return lista;
-}
+	let lista = [];
+	const peliculas = listaPelis.peliculas;
+	if (texto === "title") {
+		for (let i = 0; i < peliculas.length; i++) {
+			lista.push(peliculas[i].title);
+		}
+		lista.sort();
+	} else if (texto === "rating") {
+		lista = peliculas.map((p) => ({ title: p.title, rating: p.rating })); // crea un array de objetos con título y rating
+		lista.sort((a, b) => b.rating - a.rating); // ordena de mayor a menor según el rating
+		listaFix = lista.map((p) => ({
+			title: p.title,
+			rating: p.rating.toFixed(0) + "%",
+		}));
+		return listaFix;
+	} else if (texto === "year") {
+		lista = peliculas.map((p) => ({ title: p.title, year: p.year }));
+		lista.sort((a, b) => b.year - a.year);
+	} else if (texto === "runtime") {
+		const listaOrdenada = peliculas.map((p) => {
+			const duracion = p.runtime;
+			const partes = duracion.split(" "); // divide el string en dos partes: "1h" y "32m"
+			const horas = parseInt(partes[0]); // extrae el número de horas y conviértelo en un entero
+			const minutos = parseInt(partes[1]); // extrae el número de minutos y conviértelo en un entero
+			const minutosTotales = horas * 60 + minutos; // calcula el total de minutos
+			return { title: p.title, minutosTotales: minutosTotales };
+		}); // crea una lista de objetos que incluye el título de la película y la duración total en minutos
+		listaOrdenada.sort((a, b) => a.minutosTotales - b.minutosTotales); // ordena la lista según la duración total en minutos
+		const listaFix = listaOrdenada.map((p) => ({
+			title: p.title,
+			runtime: `${Math.floor(p.minutosTotales / 60)}h ${
+				p.minutosTotales % 60
+			}m`,
+		})); // crea una lista de objetos que incluye el título de la película y la duración en formato "Xh Ym"
+		return listaFix;
+	} else {
+		console.error(
+			`Valor "${texto}" es invalido. Por favor ingresar title, rating, year, runtime.`
+		);
+		return null;
+	}
+	return lista;
+};
 
 //Tengo que darle el titulo de la peli y entregarme los resultados similares
 exports.search = (texto) => {
-    const peliculas = listaPelis.peliculas;
-    if (texto === undefined) {
-        console.error("Error. Se esperaba algun valor.");
-        return null;
-    } else {
-        let lista = []
-        lista = peliculas.filter((p) => p.title.toLowerCase().includes(texto.toLowerCase()));
-        return lista;
-    }
-}
+	const peliculas = listaPelis.peliculas;
+	if (texto === undefined) {
+		console.error("Error. Se esperaba algun valor.");
+		return null;
+	} else {
+		let lista = [];
+		lista = peliculas.filter((p) =>
+			p.title.toLowerCase().includes(texto.toLowerCase())
+		);
+		return lista;
+	}
+};
 
 exports.searchByTag = (tag) => {
-    let lista = [];
-    const peliculas = listaPelis.peliculas;
-    if (tag === undefined) {
-        console.error("Error. Se esperaba un error.")
-        return null;
-    } else {
-        lista = peliculas.filter((p) => p.tags.includes(tag.toLowerCase()));
-        return lista;
-    };  
-}
+	let lista = [];
+	const peliculas = listaPelis.peliculas;
+	if (tag === undefined) {
+		console.error("Error. Se esperaba un error.");
+		return null;
+	} else {
+		lista = peliculas.filter((p) => p.tags.includes(tag.toLowerCase()));
+		return lista;
+	}
+};
